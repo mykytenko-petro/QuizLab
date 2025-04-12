8202dc2c82d1d8f9bd5370e3633e989e04d99114
mykytenko-petro <mikitenkot8@gmail.com>
2025-04-12 22:29:39 +0300

    update requirements.txt

--- Project/urls.py
+++ Project/urls.py
@@ -1,7 +1,6 @@
 import home_app
 import create_quiz_app
 import registration_app
-import login_app
 from .settings import project
 
 project.add_url_rule(
@@ -17,8 +16,8 @@ project.add_url_rule(
     view_func = registration_app.render_registration,
     methods = ['GET','POST']
 )
-project.add_url_rule(
-    rule = '/login',
-    view_func = login_app.render_login,
-    methods = ['GET','POST']
-)
+# project.add_url_rule(
+#     rule = '/login',
+#     view_func = login_app.render_login,
+#     methods = ['GET','POST']
+# )
