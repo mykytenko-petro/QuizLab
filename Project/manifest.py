e33a59ae737cb98c656faac03e45dcbf7de36041
mykytenko-petro <mikitenkot8@gmail.com>
2025-06-09 19:58:34 +0300

    delete typescript

--- Project/manifest.py
+++ Project/manifest.py
@@ -1,15 +1,17 @@
 import os
 import dotenv
 
-DOTENV_PATH = os.path.abspath(os.path.join(__file__, "..", "..", ".env"))
-MIGRATIONS_PATH = os.path.abspath(os.path.join(__file__, '..', "migrations"))
-
 def assemble():
+    DOTENV_PATH = os.path.abspath(os.path.join(__file__, "..", "..", ".env"))
+
     if os.path.exists(path= DOTENV_PATH):
         dotenv.load_dotenv(dotenv_path= DOTENV_PATH)
 
+    # database
+    MIGRATIONS_PATH = os.path.abspath(os.path.join(__file__, '..', "migrations"))
+
     if not os.path.exists(path= MIGRATIONS_PATH):
-        os.system(os.environ["DB_INIT"])
+        os.system("flask --app Project db init")
     
-    os.system(os.environ["DB_MIGRATE"])
-    os.system(os.environ["DB_UPGRADE"])
+    os.system("flask --app Project db migrate")
+    os.system("flask --app Project db upgrade")
