8721aba4f1b1f54bc88f0e5df82847f112ff3517
Egor <139051554+EgorGalkinORG@users.noreply.github.com>
2025-08-22 14:25:31 +0300

    No style dashboard

--- home/app.py
+++ home/app.py
@@ -7,3 +7,11 @@ homeApp = flask.Blueprint(
     static_folder="static",
     static_url_path="/home_app/static",
 )
+
+dashboardApp = flask.Blueprint(
+    name="dashboard",
+    import_name="home",
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/home_app/static",
+)
