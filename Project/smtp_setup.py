f0d41e8e43a5c6c01cc6938997d8070ac28e9811
Tymofii Zelenyi <timagreen2010@gmail.com>
2025-08-24 13:41:56 +0200

    delete later

--- Project/smtp_setup.py
+++ Project/smtp_setup.py
@@ -7,9 +7,9 @@ from .settings import project
 
 project.config['MAIL_SERVER'] = 'smtp.gmail.com'
 project.config['MAIL_PORT'] = 587
-project.config['MAIL_USERNAME'] = os.getenv("EMAIL_USERNAME")
-project.config['MAIL_PASSWORD'] = os.getenv("EMAIL_PASSWORD")
-project.config['MAIL_DEFAULT_SENDER'] = os.getenv("EMAIL_USERNAME")
+project.config['MAIL_USERNAME'] = os.environ["EMAIL_USERNAME"]
+project.config['MAIL_PASSWORD'] = os.environ["EMAIL_PASSWORD"]
+project.config['MAIL_DEFAULT_SENDER'] = os.environ["EMAIL_USERNAME"]
 project.config['MAIL_USE_SSL'] = False
 project.config['MAIL_USE_TLS'] = True
 
@@ -17,6 +17,7 @@ mail = flask_mail.Mail(app=project)
 
 def send_email(subject : str, recipients : list, *agrs, **kwargs):
     msg = flask_mail.Message(
+        
         subject= subject,
         recipients= recipients,
         *agrs,
