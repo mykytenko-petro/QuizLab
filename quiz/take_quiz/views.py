edc5afafa48de49dfe90ddad1fd8fa1e726ee5b8
Egor <139051554+EgorGalkinORG@users.noreply.github.com>
2025-08-30 18:43:52 +0300

    style+functional

--- quiz/take_quiz/views.py
+++ quiz/take_quiz/views.py
@@ -1,11 +1,10 @@
 from Project.utils import page_config
 from ..models import Quiz
-from user.models import User
 from flask import jsonify
-
 
 @page_config("start_quiz.html")
 def render_start_quiz(id):
+    from user.models import User
     quiz = Quiz.query.filter_by(id=id).first()
     creator_name = User.query.filter_by(id=quiz.owner_id).first()
     return {"quiz": quiz, "creator_name": creator_name.login}
